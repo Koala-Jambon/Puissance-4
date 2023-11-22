c441b5a8e67cce1b5b9eda293665a81585c3a86a
Bugxit <101186137+Bugxit@users.noreply.github.com>
2023-11-22 09:18:47 +0000

    creation du  board

--- api.py
+++ api.py
@@ -1,13 +1,32 @@
+import itertools
+
 def create_board():
     board = [[0 for x in range(7)] for y in range(6)]
     return board
 
 def check_column(column_number):
     if board[5][column_number] == 0:
-        n = 5
-        while board[n][column_number] == 0:
-            n += -1
-        board[n+1][column_number] = color_turn
-        return board
+        return True
     else:
         return False
+
+def place_token(column_number):
+    n = 5
+    while board[n][column_number] == 0:
+        n += -1
+    board[n+1][column_number] = color_turn
+    return board
+
+def check_victory_h():
+    winner = 0
+    for check_x, check_y in tool.product(range(4), range(6))
+        n = 0
+        stop = False
+        while board[check_y][check_x+n] == board[check_y][check_x] and stop == False:
+            if n != 4:
+                n += 1
+            else:
+                stop = True
+                winner = board[check_y][check_x]
+    return winner
+        
