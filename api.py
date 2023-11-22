2f18202874d2e8ef616e7318db8c862217e90aba
Bugxit <101186137+Bugxit@users.noreply.github.com>
2023-11-22 08:52:07 +0000

    creation du  board

--- api.py
+++ api.py
@@ -2,4 +2,12 @@ def create_board():
     board = [[0 for x in range(7)] for y in range(6)]
     return board
 
-print(create_board())
+def check_column(column_number):
+    if board[5][column_number] == 0:
+        n = 5
+        while board[n][column_number] == 0:
+            n += -1
+        board[n+1][column_number] = color_turn
+        return board
+    else:
+        return "wrong moove"
