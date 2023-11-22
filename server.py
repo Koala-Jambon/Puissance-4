6ddf42b6e19413e1b720ec8ad08ffe898c4e0ae6
timtonix <timtonic@outlook.fr>
2023-11-22 11:24:42 +0100

    ajout lobby

--- server.py
+++ server.py
@@ -6,12 +6,23 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(("", 62222))
 sock.listen()
 
-client, client_address = sock.accept()
-print(f"Un nouveau gars est arrivé : {client_address}")
-data = client.recv(1024).decode("utf-8")
-data = data.split()
-if data[0] == "/lobby":
-    print(f"Ajout de {data[1]} au lobby")
-    client.send(f"{data[1]} is connected to the lobby".encode("utf-8"))
-    client.close()
-    sock.close()
+clients = []
+
+def handle_client(client, client_address):
+    print(f"Un nouveau gars est ar  rivé : {client_address}")
+    data = client.recv(1024).decode("utf-8")
+    data = data.split()
+    if data[0] == "/lobby":
+        print(f"Ajout de {data[1]} au lobby")
+        client.send(f"{data[1]} is connected to the lobby".encode("utf-8"))
+    else:
+        client.close()
+
+
+error = False
+while not error:
+    client, client_address = sock.accept()
+    print("GUY")
+    clients.append(threading.Thread(target=handle_client, args=(client, client_address)).run())
+
+
