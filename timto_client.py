dbf8a08eedb3e7236f41c5f71db88660a1cb6578
timtonix <timtonic@outlook.fr>
2023-12-01 17:32:02 +0100

    amélioration de l'interface client.py

--- timto_client.py
+++ timto_client.py
@@ -9,15 +9,16 @@ import api
 
 size = 1.5
 
+
 class App:
-    
+
     def __init__(self, player_ip_list, player_ip, board, player_turn_ip):
         self.game = api.Game(player_ip_list, board, player_turn_ip)
         self.player_number = self.game.ip_to_number(player_ip)
         self.choice_position = 0
-        pyxel.init(int(1920/size), int(1080/size), title = f"{player_ip}")
+        pyxel.init(int(1920 / size), int(1080 / size), title=f"{player_ip}")
         pyxel.run(self.in_game_update, self.draw)
-    
+
     def update(self):
         self.in_game_update()
 
@@ -37,20 +38,21 @@ class App:
                     print(f'{self.game.number_to_ip(getattr(self.game, func)())} a gagné !')
                     pyxel.quit()
                     exit()
-             
+
     # Checks if the player has played/received a moove
     def in_game_update(self):
         if (self.game.player_turn_number == self.player_number):
             if pyxel.btnp(pyxel.KEY_RIGHT) and self.choice_position in [0, 1, 2, 3, 4, 5, 6]:
                 self.choice_position += 1
-                #Envoie (self.choice_position-1) au serveur
+                # Envoie (self.choice_position-1) au serveur
             elif pyxel.btnp(pyxel.KEY_LEFT) and self.choice_position in [2, 3, 4, 5, 6, 7]:
                 self.choice_position += -1
-                #Envoie (self.choice_position-1) au serveur
-            elif pyxel.btnp(pyxel.KEY_DOWN) and self.choice_position != 0 and self.game.check_column(self.choice_position - 1) == True:
+                # Envoie (self.choice_position-1) au serveur
+            elif pyxel.btnp(pyxel.KEY_DOWN) and self.choice_position != 0 and self.game.check_column(
+                    self.choice_position - 1) == True:
                 self.game.drop_piece(self.choice_position - 1)
-                #Envoie (self.choice_position-1) au serveur et récupère toute les infos du serv
-                client.send(f"/play {self.choice_position-1}".encode("utf-8"))
+                # Envoie (self.choice_position-1) au serveur et récupère toute les infos du serv
+                client.send(f"/play {self.choice_position - 1}".encode("utf-8"))
                 # On attend la réponse du serveur
                 data = client.recv(4096).decode("utf-8")
                 data = json.loads(data)
@@ -77,26 +79,28 @@ class App:
             # On change le joueur qui doit jouer
             self.game.change_player_turn()
 
-    # Draws the board       
+    # Draws the board
     def draw_board(self):
         for draw_x, draw_y in tool.product(range(7), range(6)):
-            pyxel.rect((150 * draw_x + 435)/size, (930 - 150 * draw_y)/size, 150/size, 150/size, 1)
+            pyxel.rect((150 * draw_x + 435) / size, (930 - 150 * draw_y) / size, 150 / size, 150 / size, 1)
             if self.game.board[draw_y][draw_x] == 0:
-                pyxel.circ((150 * draw_x + 510)/size, (1005 - 150 * draw_y)/size, 70/size, 0)
+                pyxel.circ((150 * draw_x + 510) / size, (1005 - 150 * draw_y) / size, 70 / size, 0)
             if self.game.board[draw_y][draw_x] == 1:
-                pyxel.circ((150 * draw_x + 510)/size, (1005 - 150 * draw_y)/size, 70/size, 8)
+                pyxel.circ((150 * draw_x + 510) / size, (1005 - 150 * draw_y) / size, 70 / size, 8)
             if self.game.board[draw_y][draw_x] == 2:
-                pyxel.circ((150 * draw_x + 510)/size, (1005 - 150 * draw_y)/size, 70/size, 10)
+                pyxel.circ((150 * draw_x + 510) / size, (1005 - 150 * draw_y) / size, 70 / size, 10)
         if self.game.player_turn_number == self.player_number:
-            pyxel.circ((150 * (self.choice_position - 1) + 510)/size, 75/size, 70/size, 2*(self.player_number-1)+8)
+            pyxel.circ((150 * (self.choice_position - 1) + 510) / size, 75 / size, 70 / size,
+                       2 * (self.player_number - 1) + 8)
+
 
 def connect_to_server(action):
     global data, client
     if action[0].lower() == "create":
         action = '/party'
     else:
-        action =  f'/join {action[1]}'
-    
+        action = f'/join {action[1]}'
+
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Connexion au serveur...")
     client.connect(("127.0.0.1", 62222))
@@ -120,11 +124,12 @@ def connect_to_server(action):
     print(data)
     data = json.loads(data)
 
+
 os.system('clear')
 action = input("Que voulez-vous faire ? (Create/Join;Game Number)\n").split(";")
 connect_to_server(action)
 
-App(data["joueurs"],# Ip List
-    data["you"],# Ip of the computer which is running this code
-    data["board"],# Current state of the board(normally it's blank)
-    data["tour"])# Ip of the player who has to play
+App(data["joueurs"],  # Ip List
+    data["you"],  # Ip of the computer which is running this code
+    data["board"],  # Current state of the board(normally it's blank)
+    data["tour"])  # Ip of the player who has to play
