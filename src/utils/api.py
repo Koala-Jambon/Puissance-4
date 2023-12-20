075b175119a7b4ec47db8bec51d933a5d2972748
timtonix <timtonic@outlook.fr>
2023-12-20 09:10:54 +0100

    server in main

--- src/utils/api.py
+++ src/utils/api.py
@@ -1,9 +1,10 @@
 import itertools as tool
 
 
-#Return le teableau de jeu
+# Return le teableau de jeu
 def board():
     return [[0 for _ in range(7)] for _ in range(6)]
+
 
 class Game:
     def __init__(self, player_ip_list, board, player_turn_ip):
@@ -23,84 +24,92 @@ class Game:
             return 1
         else:
             return 2
-            
-    #Return l'IP du joueur en fonction de son numéro
+
+    # Return l'IP du joueur en fonction de son numéro
     def number_to_ip(self, player_number):
-        return self.player_ip_list[player_number-1]
-    
-    #Return si la colone column_number est libre ou non
+        return self.player_ip_list[player_number - 1]
+
+    # Return si la colone column_number est libre ou non
     def check_column(self, column_number):
         if self.board[5][column_number] == 0:
             return True
         else:
             return False
-    
-    #Return le tableau avec le jeton placé à la bonne hauteur dans la colone column_number
+
+    # Return le tableau avec le jeton placé à la bonne hauteur dans la colone column_number
     def drop_piece(self, column_number):
         n = 5
-        while self.board[n][column_number] == 0 and n !=-1:
+        while self.board[n][column_number] == 0 and n != -1:
             n += -1
-        self.board[n+1][column_number] = self.player_turn_number
+        self.board[n + 1][column_number] = self.player_turn_number
         self.change_player_turn()
         return self.board
-    
-    #Check si il y a 4 jetons alignés horizontalement (-) : Ne Return pas si il n'y a aucun gagnant pour le moment ; 1 Si le 1 a gagné ; 2 Si le 2 a gagné.
+
+    # Check si il y a 4 jetons alignés horizontalement (-) : Ne Return pas si il n'y a aucun gagnant pour le moment ; 1 Si le 1 a gagné ; 2 Si le 2 a gagné.
     def check_victory_h(self):
         winner = 0
         for check_x, check_y in tool.product(range(4), range(6)):
             n = 0
-            while self.board[check_y][check_x+n] == self.board[check_y][check_x] and self.board[check_y][check_x] != 0:
+            while self.board[check_y][check_x + n] == self.board[check_y][check_x] and self.board[check_y][
+                check_x] != 0:
                 if n != 3:
                     n += 1
                 else:
                     winner = self.board[check_y][check_x]
-                    self.victory_reason = ((check_x, check_y), (check_x+1, check_y), (check_x+2, check_y), (check_x+3, check_y))
+                    self.victory_reason = (
+                    (check_x, check_y), (check_x + 1, check_y), (check_x + 2, check_y), (check_x + 3, check_y))
                     return int(winner)
         return False
-    
-    #Check si il y a 4 jetons alignés verticalement (|) : Ne Return pas si il n'y a aucun gagnant pour le moment ; 1 Si le 1 a gagné ; 2 Si le 2 a gagné.
+
+    # Check si il y a 4 jetons alignés verticalement (|) : Ne Return pas si il n'y a aucun gagnant pour le moment ; 1 Si le 1 a gagné ; 2 Si le 2 a gagné.
     def check_victory_v(self):
         winner = 0
         for check_x, check_y in tool.product(range(7), range(3)):
             n = 0
-            while self.board[check_y+n][check_x] == self.board[check_y][check_x] and self.board[check_y][check_x] != 0:
+            while self.board[check_y + n][check_x] == self.board[check_y][check_x] and self.board[check_y][
+                check_x] != 0:
                 if n != 3:
                     n += 1
                 else:
                     winner = self.board[check_y][check_x]
-                    self.victory_reason = ((check_x, check_y), (check_x, check_y+1), (check_x, check_y+2), (check_x, check_y+3))
+                    self.victory_reason = (
+                    (check_x, check_y), (check_x, check_y + 1), (check_x, check_y + 2), (check_x, check_y + 3))
                     return int(winner)
         return False
-    
-    #Check si il y a 4 jetons alignés diagonalement (/) : Ne Return pas si il n'y a aucun gagnant pour le moment ; 1 Si le 1 a gagné ; 2 Si le 2 a gagné.      
+
+    # Check si il y a 4 jetons alignés diagonalement (/) : Ne Return pas si il n'y a aucun gagnant pour le moment ; 1 Si le 1 a gagné ; 2 Si le 2 a gagné.
     def check_victory_dp(self):
         winner = 0
         for check_x, check_y in tool.product(range(4), range(3)):
             n = 0
-            while self.board[check_y+n][check_x+n] == self.board[check_y][check_x] and self.board[check_y][check_x] != 0:
+            while self.board[check_y + n][check_x + n] == self.board[check_y][check_x] and self.board[check_y][
+                check_x] != 0:
                 if n != 3:
                     n += 1
                 else:
                     winner = self.board[check_y][check_x]
-                    self.victory_reason = ((check_x, check_y), (check_x+1, check_y+1), (check_x+2, check_y+2), (check_x+3, check_y+3))
+                    self.victory_reason = ((check_x, check_y), (check_x + 1, check_y + 1), (check_x + 2, check_y + 2),
+                                           (check_x + 3, check_y + 3))
                     return int(winner)
         return False
-    
-    #Check si il y a 4 jetons alignés diagonalement (\) : Ne Return pas si il n'y a aucun gagnant pour le moment ; 1 Si le 1 a gagné ; 2 Si le 2 a gagné.
+
+    # Check si il y a 4 jetons alignés diagonalement (\) : Ne Return pas si il n'y a aucun gagnant pour le moment ; 1 Si le 1 a gagné ; 2 Si le 2 a gagné.
     def check_victory_dm(self):
         winner = 0
-        for check_x, check_y in tool.product([3,4,5,6], range(3)):
+        for check_x, check_y in tool.product([3, 4, 5, 6], range(3)):
             n = 0
-            while self.board[check_y+n][check_x-n] == self.board[check_y][check_x] and self.board[check_y][check_x] != 0:
+            while self.board[check_y + n][check_x - n] == self.board[check_y][check_x] and self.board[check_y][
+                check_x] != 0:
                 if n != 3:
                     n += 1
                 else:
                     winner = self.board[check_y][check_x]
-                    self.victory_reason = ((check_x, check_y), (check_x-1, check_y+1), (check_x-2, check_y+2), (check_x-3, check_y+3))
+                    self.victory_reason = ((check_x, check_y), (check_x - 1, check_y + 1), (check_x - 2, check_y + 2),
+                                           (check_x - 3, check_y + 3))
                     return int(winner)
         return False
-    
-    #Check si la game doit s'arrêter, soit à cause d'une win ou d'une tie ; Return True si la game doit s'arrêter ; Return False si la Game doit continuer 
+
+    # Check si la game doit s'arrêter, soit à cause d'une win ou d'une tie ; Return True si la game doit s'arrêter ; Return False si la Game doit continuer
     def check_endgame(self):
         for func in ["check_victory_h", "check_victory_v", "check_victory_dp", "check_victory_dm"]:
             if getattr(self, func)():
@@ -112,15 +121,15 @@ class Game:
     # Returns the reason of the victory
     def victory_reason(self):
         return self.victory_coords
-                 
-    #Return True si le plateau est plein ; False si le plateau a toujours des cases libres
+
+    # Return True si le plateau est plein ; False si le plateau a toujours des cases libres
     def check_tie(self):
         for column_check_number in range(7):
             if self.board[5][column_check_number] == 0:
                 return False
         return True
-    
-    #Return le numéro du joueur qui doit jouer ne fonction du joueur qui vient de jouer:
+
+    # Return le numéro du joueur qui doit jouer ne fonction du joueur qui vient de jouer:
     def change_player_turn(self):
         if self.player_turn_number == 1:
             self.player_turn_number = 2
